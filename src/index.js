2110b0d351d1b71f55b0ee766a324b134cf37628
erwin foxtree <foxtree@users.noreply.github.com>
2021-01-22 14:45:25 -0500

    src/I18nProvider.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {
   RecoilRoot,
-  // atom,
-  // selector,
-  // useRecoilState,
-  // useRecoilValue,
 } from 'recoil';
 
 import { ThemeProvider, rootTheme } from './themes';
+import I18nProvider from './I18nProvider';
 import './css/Colors.css';
 import './css/Fonts.css';
 
@@ -19,13 +16,15 @@ import DiscoveryApp from './components/DiscoveryApp';
 ReactDOM.render(
   <RecoilRoot>
     <ThemeProvider theme={rootTheme}>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={ParticipantList} />
-          <Route path="/participant/:participantId/:activeView?" component={DiscoveryApp} />
-          <Route path="/uploaded/:id" component={DiscoveryApp} />
-        </Switch>
-      </Router>
+      <I18nProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={ParticipantList} />
+            <Route path="/participant/:participantId/:activeView?" component={DiscoveryApp} />
+            <Route path="/uploaded/:id" component={DiscoveryApp} />
+          </Switch>
+        </Router>
+      </I18nProvider>
     </ThemeProvider>
   </RecoilRoot>,
   document.getElementById('root'),
